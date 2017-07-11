088826b6d44ebe958c9949e5c5c253643160edb2
Gniewogniew <jakubbajer83@gmail.com>
2017-07-11 21:06:34 +0200

    Update script.js

--- script.js
+++ script.js
@@ -2,10 +2,10 @@ document.getElementById("menu").addEventListener("change", MyFunction);
 
 var element = document.getElementById("content");
 
-function MyFunction(event) {
+    function MyFunction(event) {
     var style = event.target.id;
     var value = event.target.value;
-   element.style[style] = value;  
-    
-if (event.target.dataset.unit){element.style[style] = value+=event.target.dataset.unit}
+
+if (event.target.dataset.unit){value+=event.target.dataset.unit}
+    element.style[style] = value; 
 }
